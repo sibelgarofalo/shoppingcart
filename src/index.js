7a31a50a44464d023936a68476d1fa2edeafc9b0
Sibel <sblgrfl@gmail.com>
2022-09-20 10:27:14 +0200

    Built navigation and use Router in Login

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 
 import Routing from './components/routing/routing';
 
+import './index.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,8 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+
 import './login.scss';
+
 function Login() {
+  const navigate = useNavigate();
+
+
+  const onResetClick = () => {
+    navigate('/password-reset');
+  }
+
+  const onRegisterClick = () => {
+    navigate('/register');
+  }
+
+  const onCancelClick = () => {
+
+  }
+
+  const onLoginClick = () => {
+
+  }
 
   return (
-    <div className="login">Login</div>
+    <div className="login">
+      <form>
+        <h3>Login user</h3>
+        <div className="input-group">
+          <label>Email address</label>
+          <input type="Email" placeholder='Insert your Email' />
+        </div>
+        <div className="input-group">
+          <label>Password</label>
+          <input type="Password" placeholder='Insert your Password' />
+        </div>
+        <div className='buttons-group'>
+          <button onClick={onResetClick} className='ghost'>Reset</button>
+          <button onClick={onRegisterClick} className='ghost'>Register</button>
+          <button onClick={onCancelClick} className='cancel'>Cancel</button>
+          <button onClick={onLoginClick} className='positive'>Login</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
